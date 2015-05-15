6b46d8aa046eb3c92f9c81492f8b6b85727adaf5
India Dearlove <india_dearlove@yahoo.co.uk>
2015-05-15 22:14:36 +0100

    colours made from jquery

--- javascript.js
+++ javascript.js
@@ -1,2 +1,6 @@
 $(document).ready(function(){
+  $("#white").css("background-color", "white");
+  $("#blue").css("background-color", "blue");
+  $("#yellow").css("background-color", "yellow");
+  $("#green").css("background-color", "green");
 });
